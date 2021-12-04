e1e17414f758569f8772df971dce225a57eb6998
Videoangel33 <videoangel33@gmail.com>
2021-12-04 00:32:50 -0500

    Actualize clientes hbs, productos hbs, reportes hbs y reportesVentas js
    Marque todos los cambios con comentarios, sobre todo mirar la seccion de reportes (las tablas) para mirar si estan bien

---
+++ Front/ProyectoFinal-Ciclo4/src/js/reporteVentas.js
@@ -0,0 +1,62 @@
+///////////////////////////////////////////////////////////////////////////////////////////
+const express = require('express');
+const router = express.Router();
+let tabla = [{}];
+
+
+router.get('/cargarTabla', isAuthenticated,function (req, res) {
+    let codigo = document.getElementById("Codigo");
+    res.redirect('/productos');
+    let datos = req.body();
+    agregarElemento(datos);
+
+});
+
+function agregarElemento(datos){
+    for(let add of datos){tabla.push(datos)}
+    crearTabla(tabla);
+};
+
+let crearTabla = function (lista){
+    let stringTabla = "<tr><th>Cliente</th><th>Cedula</th><th>Fecha Venta</th><th>Codigo del Articulo</th><th>Valor Total de la Venta</th></tr>";
+    for(let element of lista){
+        let fila = "<tr> <td>";
+        fila += element.cliente;
+        fila += "</td>";
+
+        fila += "<td>";
+        fila += element.cedula;
+        fila += "</td>";
+
+        fila += "<td>";
+        fila += element.fecha;
+        fila += "</td>";
+
+        fila += "<td>";
+        fila += element.codigo;
+        fila += "</td>";
+
+        fila += "<td>";
+        fila += element.valorTotal;
+        fila += "</td>";
+
+        fila += "</tr>";
+
+        stringTabla += fila;
+    }
+    return stringTabla;
+};
+
+document.getElementById("tablaVentas").innerHTML = crearTabla(cliente);
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+//router.get('/cargarTabla', isAuthenticated,function (req, res) {
+    //let codigo = document.getElementById("Codigo");
+    // let url = "/productos";
+   // fectch
+//});
+
+//router.get('/users/logout', function (req, res) {
+ //   req.logout();
+ //   res.redirect('/');
+//});
